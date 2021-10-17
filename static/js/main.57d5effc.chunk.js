de784355ac4a927a64971f4a805aaf3349a20668
Satyam1203 <nakulbegana191170@gmail.com>
2021-10-17 15:09:43 +0530

    Updates

---
+++ static/js/main.57d5effc.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpsatyam1203.github.io"]=this["webpackJsonpsatyam1203.github.io"]||[]).push([[0],{11:function(t,n,e){"use strict";e.r(n);var a=e(1),c=e.n(a),i=e(3),s=e.n(i),o=(e(8),e(9),e(0));var r=function(){return Object(o.jsx)("div",{className:"App",children:"Satyam Lachhwani"})};s.a.render(Object(o.jsx)(c.a.StrictMode,{children:Object(o.jsx)(r,{})}),document.getElementById("root"))},8:function(t,n,e){},9:function(t,n,e){}},[[11,1,2]]]);
+//# sourceMappingURL=main.57d5effc.chunk.js.map
