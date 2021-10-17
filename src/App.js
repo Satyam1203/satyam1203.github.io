d508c0659bedca3d8756048cc587cd6a41e4aa03
Satyam1203 <nakulbegana191170@gmail.com>
2021-10-17 15:19:14 +0530

    add a new dependency

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import { TalkBox } from "react-talkbot";
 
 function App() {
-  return <div className="App">Satyam Lachhwani</div>;
+  return (
+    <div className="App">
+      <h1>Satyam Lachhwani</h1>
+      <TalkBox />
+    </div>
+  );
 }
 
 export default App;
